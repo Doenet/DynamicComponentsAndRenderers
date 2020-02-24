aeba09096b963c0b9a18003840ee6f8bc51cc6c9
Kevin Charles <char0042@umn.edu>
2020-02-24 15:26:50 -0600

    Component Reg in Separate File

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { componentReg } from './Reg';
 
-// all lower case componentName:[bundle which includes the component,name of import]
-const componentReg = {
-  first: ['Numbers.js','First'],
-  second: ['Numbers.js','Second'],
-  third: ['Numbers.js','TheThirdOne'],
-  fourth: ['Numbers.js','Fourth'],
-  a: ['Letters.js','A'],
-  b: ['Letters.js','B'],
-  dog: ['Dog.js','Dog'],
-  }
 
 
 class App extends Component {
---
+++ src/Reg.js
@@ -0,0 +1,10 @@
+// all lower case componentName:[bundle which includes the component,name of import]
+export const componentReg = {
+  first: ['Numbers.js','First'],
+  second: ['Numbers.js','Second'],
+  third: ['Numbers.js','TheThirdOne'],
+  fourth: ['Numbers.js','Fourth'],
+  a: ['Letters.js','A'],
+  b: ['Letters.js','B'],
+  dog: ['Dog.js','Dog'],
+  }
